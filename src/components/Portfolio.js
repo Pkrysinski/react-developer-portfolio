537ecc9eb5ac735e0d576be9594d9e3394631ab7
Peter Krysinski <pakrysin@gmail.com>
2023-04-06 10:59:35 -0600

    Adding a little more content to the Portfolio.js section, also trying to add an image to reference

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,45 +1,87 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-// import Button from '@mui/material/Button';
+import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function Portfolio() {
 
     const projects = [
-        { id: 1, title: "Project 1", desc: "", website: "", github: "", img: ""},
-        { id: 2, title: "Project 2", desc: "", website: "", github: "", img: ""},
-        { id: 3, title: "Project 3", desc: "", website: "", github: "", img: ""},
-        { id: 4, title: "Project 4", desc: "", website: "", github: "", img: ""},
-        { id: 5, title: "Project 5", desc: "", website: "", github: "", img: ""},
-        { id: 6, title: "Project 6", desc: "", website: "", github: "", img: ""}
+        {
+            id: 1,
+            title: "MVC Tech Blog",
+            desc: "The purpose of this app is to showcase our understanding of the Model-View-Controller (MVC) architecture through expressing our knowledge of why separation of concerns in this consideration is a great practive to follow.",
+            website: "https://mvc-tech-blog-app-production.up.railway.app/",
+            github: "https://github.com/Pkrysinski/MVC-Tech-Blog-app",
+            img: "./images/mvc-tech-blog-img.jpg", //TODO: Image won't load, only the alt text.
+            alt: "mvc-tech-blog.jpg"
+        },
+        {
+            id: 2,
+            title: "Project 2",
+            desc: "",
+            website: "",
+            github: "",
+            img: ""
+        },
+        {
+            id: 3,
+            title: "Project 3",
+            desc: "",
+            website: "",
+            github: "",
+            img: ""
+        },
+        {
+            id: 4,
+            title: "Project 4",
+            desc: "",
+            website: "",
+            github: "",
+            img: ""
+        },
+        {
+            id: 5,
+            title: "Project 5",
+            desc: "",
+            website: "",
+            github: "",
+            img: ""
+        },
+        {
+            id: 6,
+            title: "Project 6",
+            desc: "",
+            website: "",
+            github: "",
+            img: ""
+        }
     ]
 
     return (
         <section id="portfolio" style={{ backgroundColor: "#ffe9ec", color: "black" }}>
             <div>
-                <h1>
-                    Here is my portfolio
-                </h1>
-                <div style={{ display: "flex", border: "2px solid grey" }}>
+                <div style={{ display: "flex", border: "2px solid grey", justifyContent:"space-between" }}>
                     {projects.map(project => {
                         return (
-                            <Card style={{ width: "18em" }} key={project.id}>
-                                <CardContent>
-                                    <Typography sx={{ fontSize: 14 }} gutterBottom>
-                                        {project.title}
-                                    </Typography>
-                                    <Typography variant="h5" component="div">
-
-                                    </Typography>
-                                    <Typography sx={{ mb: 1.5 }} >
-
-                                    </Typography>
-                                    <Typography variant="body2">
-
-                                    </Typography>
-                                </CardContent>
+                            <Card sx={{ maxWidth: 345 }} key={project.id}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        height="140"
+                                        image={project.img}
+                                        alt={project.alt}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {project.title}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {project.desc}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
                             </Card>
                         )
                     })}
