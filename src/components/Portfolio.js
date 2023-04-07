6c31dbd2170feaf3658cde4316a6d80e47f25121
Peter Krysinski <pakrysin@gmail.com>
2023-04-06 18:31:58 -0600

    Need to change .jpg to .JPG in the img refereces in the portfolio cards.  It didn't matter when I was testing locally, but apparently it does when deployed!

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -21,8 +21,8 @@ function Portfolio() {
             desc: "The purpose of this app is to showcase our understanding of the Model-View-Controller (MVC) architecture.",
             website: "https://mvc-tech-blog-app-production.up.railway.app/",
             github: "https://github.com/Pkrysinski/MVC-Tech-Blog-app",
-            img: "mvc-tech-blog-img.jpg",
-            alt: "mvc-tech-blog.jpg"
+            img: "mvc-tech-blog-img.JPG",
+            alt: "mvc-tech-blog.JPG"
         },
         {
             id: 2,
@@ -30,8 +30,8 @@ function Portfolio() {
             desc: "A fun cat meme generator showcasing understanding of 3rd party API's and local storage for historical searches.",
             website: "https://pkrysinski.github.io/cat-meme-generator/",
             github: "https://github.com/Pkrysinski/cat-meme-generator",
-            img: "cat-meme-generator-img.jpg",
-            alt: "cat-meme-generator-img.jpg"
+            img: "cat-meme-generator-img.JPG",
+            alt: "cat-meme-generator-img.JPG"
         },
         {
             id: 3,
@@ -48,7 +48,8 @@ function Portfolio() {
             desc: "A simple workday scheduler app that allow users to save calendar/schedule-based events into hours of the day, with data that saves and persists in local storage. The app will be connected to the DayJS library which gives the ability to have time-based styling depending on what time of day it is (past events will be grey, current will be red, and future will be green).",
             website: "https://deft-sunshine-82cf3c.netlify.app/",
             github: "https://github.com/Pkrysinski/workday-scheduler-app",
-            img: "worksday-scheduler-app-img.JPG"
+            img: "worksday-scheduler-app-img.JPG",
+            alt: "worksday-scheduler-app-img.JPG"
         },
         {
             id: 5,
@@ -56,7 +57,8 @@ function Portfolio() {
             desc: "A quiz game app based on JavaScript fundamentals. The quiz will prompt a user to start via button, which will trigger a timer to start in the top-right via an event listener. The user will then be promped through a series of basic multiple-choice questions, and depending if the user is correct or not, will either reset the timer back to 10 seconds, or decriment it by some time. The user will also be able to view their previous high scores in the upper-left hand corner.",
             website: "https://pkrysinski.github.io/javascript-quiz-app/",
             github: "https://github.com/Pkrysinski/javascript-quiz-app",
-            img: "javascript-quiz-img.JPG"
+            img: "javascript-quiz-img.JPG",
+            alt: "javascript-quiz-img.JPG"
         },
         {
             id: 6,
